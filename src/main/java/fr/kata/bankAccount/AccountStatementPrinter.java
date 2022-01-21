a3814577e2cad2af8831b7012511cdb85b5c7bbc
Bastien <van.bastien15@gmail.com>
2022-01-21 10:40:40 +0100

    replace AccountPrinter by AccountStatementPrinter

--- src/main/java/fr/kata/bankAccount/AccountStatementPrinter.java
+++ src/main/java/fr/kata/bankAccount/AccountStatementPrinter.java
@@ -5,24 +5,24 @@ import java.math.RoundingMode;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
-public class AccountPrinter {
+public class AccountStatementPrinter {
     private static final String SEPARATOR = " ## ";
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm:ss");
 
-    private final PrintStream out;
+    private final PrintStream printer;
 
-    public AccountPrinter(PrintStream out) {
-        this.out = Objects.requireNonNull(out);
+    public AccountStatementPrinter(PrintStream printer) {
+        this.printer = Objects.requireNonNull(printer);
     }
 
-    public void print(Account account) {
-        out.println("Date ## Operation Type ## Amount ## Balance");
-        account.getStatement().getOperations().forEach(operation -> {
+    public void print(AccountStatement accountStatement) {
+        printer.println("Date ## Operation Type ## Amount ## Balance after operation");
+        accountStatement.getOperations().forEach(operation -> {
             StringBuilder lineStatement = new StringBuilder(operation.getDate().format(DATE_TIME_FORMATTER));
             lineStatement.append(SEPARATOR).append(operation.getType());
             lineStatement.append(SEPARATOR).append(operation.getAmount().setScale(2, RoundingMode.DOWN));
             lineStatement.append(SEPARATOR).append(operation.getBalance().setScale(2, RoundingMode.DOWN));
-            out.println(lineStatement);
+            printer.println(lineStatement);
         });
     }
 }
--- src/test/java/fr/kata/bankAccount/AccountStatementPrinterTest.java
+++ src/test/java/fr/kata/bankAccount/AccountStatementPrinterTest.java
@@ -14,7 +14,7 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class AccountPrinterTest {
+public class AccountStatementPrinterTest {
 
     private PrintStream printer;
     private Path tempFile;
@@ -35,7 +35,7 @@ public class AccountPrinterTest {
     @Test
     void cannot_be_initialized_with_null_printer() {
         // when
-        Executable callable = () -> new AccountPrinter(null);
+        Executable callable = () -> new AccountStatementPrinter(null);
         // then
         Assertions.assertThrows(RuntimeException.class, callable);
     }
@@ -43,14 +43,16 @@ public class AccountPrinterTest {
     @Test
     void should_print_statement_of_account() throws IOException {
         // given
-        Account account = new Account(new AccountStatement(() -> LocalDateTime.parse("2022-01-18T10:21:02"))).deposit(BigDecimal.valueOf(54.264568)).withdrawal(BigDecimal.valueOf(10));
-        AccountPrinter accountPrinter = new AccountPrinter(printer);
+        AccountStatement accountStatement = new AccountStatement(() -> LocalDateTime.parse("2022-01-18T10:21:02"))
+                .registerOperation(AccountOperationType.DEPOSIT, BigDecimal.valueOf(54.26), BigDecimal.valueOf(55.6))
+                .registerOperation(AccountOperationType.WITHDRAWAL, BigDecimal.valueOf(10), BigDecimal.valueOf(44.26));
+        AccountStatementPrinter accountPrinter = new AccountStatementPrinter(printer);
         // when
-        accountPrinter.print(account);
+        accountPrinter.print(accountStatement);
         // then
         List<String> statementLines = Files.readAllLines(tempFile);
-        Assertions.assertEquals(statementLines.get(0), "Date ## Operation Type ## Amount ## Balance");
-        Assertions.assertEquals(statementLines.get(1), "2022-01-18 10:21:02 ## DEPOSIT ## 54.26 ## 54.26");
+        Assertions.assertEquals(statementLines.get(0), "Date ## Operation Type ## Amount ## Balance after operation");
+        Assertions.assertEquals(statementLines.get(1), "2022-01-18 10:21:02 ## DEPOSIT ## 54.26 ## 55.60");
         Assertions.assertEquals(statementLines.get(2), "2022-01-18 10:21:02 ## WITHDRAWAL ## 10.00 ## 44.26");
     }
 }
